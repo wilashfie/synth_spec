b30811198422ec5a3a8665c59a89c83122b58e58
Will Ashfield <wilashfie@gmail.com>
2020-06-01 11:50:02 -0600

    initial commit

---
+++ create_arrays.py
@@ -0,0 +1,194 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import readsav
+from scipy.interpolate import interp1d
+from scipy import optimize
+from scipy.optimize import curve_fit
+from scipy.signal import savgol_filter
+from matplotlib import gridspec
+
+
+# creates syntetic Si IV - 1402.77 ang. line from PREFT simulation
+
+
+# inputs: tube, frac, logT, log10G, time (optional)
+# tube - tarr array from PREFT sim, imported into python notebook using scipy.readsav()
+# frac - ionization fraction for Si IV, created using IDL function on server
+# log10T/log10G, log of contribution function G and corresponding log of temperature T, imported from dana's email
+
+# set inital values
+line = 1403.
+mass = 28.0*1.66054e-27
+kb = 1.3807e-23
+c = 300
+h = 6.62607e-27
+flux=1.0e3 #? why this val?
+ll = np.arange(line-10,line+10,0.01)
+
+
+def create_arr(tube,frac,log10T,log10G,time=55,verbose=False):
+
+    # define arrays from tube.tarr
+    t = tube.tarr.t[time]
+    n = tube.tarr.n[time]
+    los_v = tube.tarr.v[time].T[0]
+    sm_v = -savgol_filter(los_v,3,1)
+    los_x = tube.tarr.x[time].T[0]
+    n_e = tube.tarr.epamu[time]*tube.tarr.rho[time]/1.67e-8 # number density
+    b = tube.tarr.b[time]
+    dl_e = tube.tarr.dl_e[time]
+
+    # interpolate our GofT data
+    te = 10**log10T
+    inter = interp1d(te,log10G,kind='cubic', bounds_error=False, fill_value=-10e6) #fill outide vals with large, small number
+    temp = 1e6*tube.tarr.t[time]
+    G = inter(temp)
+    G[temp<22000] = -10000 # set all log10G with low temp to large, small number (st 10^G~0)
+
+    # nei/eqi arrays at time =time
+    f_nei=frac.fraq.f_nei[0]
+    f_nei = f_nei[:,time]
+    f_eqi=frac.fraq.f_eqi[0]
+    f_eqi = f_eqi[:,time]
+
+    # interpolation arrays
+    # define subregion
+    i_min,i_max = 285,999 # left half of tube (start = 3.9s)
+    #i_min,i_max = 1294,1715 # right half of tube
+
+    t_s = t[i_min:i_max]
+    n_s = len(t_s)
+    los_v_s = los_v[i_min:i_max]
+    sm_v_s = sm_v[i_min:i_max]
+    los_x_s = los_x[i_min:i_max]
+    n_e_s = n_e[i_min:i_max]
+    b_s = b[i_min:i_max]
+    dl_e_s = dl_e[i_min:i_max]
+    G_s = G[i_min:i_max]
+    f_nei_s = f_nei[i_min:i_max]
+    f_eqi_s = f_eqi[i_min:i_max]
+
+    # interpolate
+    N=10*n_s
+    i_s = np.arange(0,n_s)
+    ii = np.arange(0,10*(n_s-1))*0.1
+
+    int_x = interp1d(i_s,los_x_s,kind='linear')#,fill_value="extrapolate")
+    int_v = interp1d(i_s,sm_v_s,kind='linear')
+    int_t = interp1d(i_s,t_s,kind='linear')
+    int_ne = interp1d(i_s,n_e_s,kind='linear')
+    int_b = interp1d(i_s,b_s,kind='linear')
+    int_dl_e = interp1d(i_s,dl_e_s,kind='linear')
+    int_G = interp1d(i_s,G_s,kind='linear')
+    int_fnei = interp1d(i_s,f_nei_s,kind='linear')
+    int_feqi = interp1d(i_s,f_eqi_s,kind='linear')
+
+    # new, interpolated arrays from tarr/tube
+    x = int_x(ii)
+    v = int_v(ii)
+    T = int_t(ii)
+    ne = int_ne(ii)
+    B = int_b(ii)
+    Dl = int_dl_e(ii)
+    g = int_G(ii)
+    nei = int_fnei(ii)
+    eqi = int_feqi(ii)
+
+    # factor to multiply GofT by to get actual emission given NEI
+    factor = nei/eqi
+    np.nan_to_num(factor, copy=False, nan=1); # replace inf values with 1 (due to zeros in eqi)
+
+    # process to create volume given per flux (per Maxwell)
+    rad=np.sqrt(flux/B/np.pi)
+    b_e=0.5*( np.roll(B, -1) + B )
+    A1=rad
+    A2=np.roll(rad,-1)
+    A2[(len(B))-1]=A2[len(B)-2]
+    #volume=(1./3.)*Dl*(A1+A2+np.sqrt(A1*A2))*1e24
+    A = flux/B # replace by area of pixel in Mm (~0.01 Mm^2)
+    A_pixel = 0.029 # Mm^2 -  pixel area: 0.33 x 0.167 arcsec (as seen on sun..)
+
+    volume = Dl*A_pixel*1e24
+    #calcualte emission measure EM
+    EM=volume*ne**2
+
+
+    # calculate prefactor to turn intensity into counts
+    photo_erg = h*c*1e6/line*1e10 #erg/photon
+    pixel_size = 12.8e-3 # in ˚A [12.8 m˚A (FUV), 25.6 m˚A (NUV)]
+    #FOV = 0.33*175*2.35e-11 # in sr 0.33 × 175 arcsec^2 SG-slit
+    dim = 19e-2
+    A_iris = 2.2e-4 # effective area FUV
+    au = 1.49e11
+    atn = A_iris/au**2
+    exp_time = 4
+    #photo_fac = pixel_size*atn*exp_time/photo_erg # converts EM*g/sig (erg/s/sr/˚A) -> photon count
+    photo_fac = pixel_size*atn*exp_time/photo_erg
+
+
+
+    # determine line broadening given combination of thermal, non-thermal, and instrumental broadenings
+    freq = c/line*1e3
+    v_inst = 3.9 # instrumental broadening (km/s)
+    sig_inst = v_inst/freq
+    v_nt = 20.0 # non-thermal broadening (km/s) [De Pontieu et al, 2015]
+    sig_nt = v_nt/freq
+    sig_th = line*np.sqrt(kb*1e6*T/mass)/(c*1e6) # thermal broadening (in ˚A)
+    sig = np.sqrt(sig_th**2+sig_nt**2+sig_inst**2) # total broadening
+
+    # create emission array(s) for each fluid element
+    nn=10*(n_s-1)
+    emissNEI = np.empty([nn,len(ll)])
+    emiss = emissNEI
+    for i in range(nn):
+        emissNEI[i,:] = photo_fac*EM[i]*factor[i]*10**g[i]/np.sqrt(2*np.pi)/sig[i]*np.exp(-(ll-line-line*v[i]/c)**2/(2*sig[i]**2))
+        emiss[i,:] = photo_fac*EM[i]*10**g[i]/np.sqrt(2*np.pi*sig[i])*np.exp(-(ll-line-line*v[i]/c)**2/(2*sig[i]**2))
+        #emiss[i,:] = EM[i]*10**g[i]/np.sqrt(2*np.pi*sig[i])*np.exp(-(ll-line-line*v[i]/c)**2/(2*sig[i]**2))
+        #emiss2[i,:] = EM[i]*10**g[i]*line/(c*1e6)*np.exp(-(ll-line-line*v[i]/c)**2/(2*sig[i]**2))
+
+    # integrate along loop
+    tot_emissNEI = np.sum(emissNEI,axis=0)
+    #tot_emissNEI /= max(tot_emissNEI) #normalize
+    tot_emiss = np.sum(emiss,axis=0)
+    #tot_emiss /= max(tot_emiss) #normalize
+
+    #generate and add noise
+    yerr = np.sqrt(tot_emissNEI)
+    noise = np.random.normal(0.0 ,size = len(tot_emissNEI)) # make gaussian noise instead.
+    error = yerr*noise
+    np.nan_to_num(error, copy=False, nan=0)
+    tot_emissNEI += error
+
+    if verbose == True:
+        print('A = ', A)
+        print('A_pixel = ', A_pixel)
+        print('ne = ', ne)
+        print('vol = ', volume)
+        print('area iris = ', A_iris)
+        print('atn = ', atn)
+        print('photo erg = ', photo_erg)
+
+    d = {'wav':ll,'spec':tot_emissNEI,'EM':EM,'x':x,'v':v,'T':T,'ne':ne}
+
+
+    # d['wav'] = ll
+    # d['spec'] = tot_emissNEI
+    # d['EM'] = np.sum(EM)
+    # d['x'] = x
+    # d['v'] = v
+    # d['T'] = T
+    # d['ne'] = ne
+
+    return d
+
+
+# plot
+#fig=plt.figure(figsize=[10,10])
+#fig, ax = plt.subplots(figsize=[10,10])
+#plt.plot(ll,tot_emissNEI)
+#plt.plot(ll,tot_emiss)
+#plt.xlim(1403,1404)
+#plt.ylabel('normalized intensity')
+#ax.set_xlabel('wavelength [$\AA$]')
+
+#fig.savefig('specI.png',bbox_inches='tight', dpi=600)
---
+++ fit2gauss.py
@@ -0,0 +1,154 @@
+import numpy as np
+from scipy import optimize
+from scipy.optimize import curve_fit
+from scipy.signal import savgol_filter
+
+
+def single_gauss_func_noder(x, *a):
+    # evaluate the single gaussian on x and return its value in f
+
+    # === the exponential factors
+    ef1 = np.exp( -0.5*( (x-a[1])/a[2] )**2 )
+
+    f = a[0]*ef1
+
+    return f
+
+
+def double_gauss_func_noder(x, *a):
+    # evaluate the double gaussian on x and return its value in f
+
+    # === the exponential factors
+    ef1 = np.exp( -0.5*( (x-a[1])/a[2] )**2 )
+    ef2 = np.exp( -0.5*( (x-a[4])/a[5] )**2 )
+
+    f = a[0]*ef1 + a[3]*ef2
+
+    return f
+
+
+def est_params(mvec, sig=0.1, dx=1.):
+#  give moments 0 - 3, as elements of mvec, estimate the parameters of 2 gaussians
+
+
+    s = mvec[3]/mvec[2]**(1.5) # the skewness
+    th = ( np.arctan(0.5*s) > (-0.49*np.pi)) < ( 0.49*np.pi )
+    f = 0.5 - 0.5*np.sin(th) # fraction of red-shifted component
+    dv = np.sqrt(mvec[2]/f/(1.0-f)) # estimate of separation  v_red - v_blue
+    #mvec[1] = f*v_red + (1.0-f)*v_blue = v_blue + f*dv
+    v_blue = mvec[1] - f*dv
+    v_red = dv + v_blue
+
+    a0 = dx*mvec[0]/(np.sqrt(2*np.pi*mvec[2]))
+
+    est = [f*a0, v_red, sig, (1.0-f)*a0, v_blue, sig]
+
+    return est
+
+
+
+
+def fit2gauss(lam, y, yerr, min_tot=0.1, chi_thr=10.0, base=0.0, verbose=False):
+
+    # min_tot= minimum intensity to try
+    # chi_thr= Chi^2 threshold
+    # base= base level subtracted when computing moments
+
+    # ==== compute moments
+
+    d = dict()
+
+    yt = ( y - base ) #> 0.0 #    a truncated version for moments
+    m0 = np.sum( yt ) #> 1.0 #    prevent problems with division
+    m1 = np.sum( yt*lam )/m0
+    m2 = np.sum( yt*(lam-m1)**2 )/m0
+    m3 = np.sum( yt*(lam-m1)**3 )/m0
+
+    moms = [ m0, m1, m2 ] #  pack for return
+
+    # if first moment is too small (nothing to fit)
+    if( m0 < min_tot ):
+        chi1g = -1.0
+        a1g = [0,0,0]
+        d['moms'] = moms
+        d['chi1g'] = chi1g
+        d['a2g'] = [0,0,0,0,0,0]
+        if verbose==True: print('ejecting!')
+        return d
+
+    # ===== do 1-Gaussian fit
+    # estimate parameters for 1-Gaussian fit
+    sd = np.sqrt(m2)
+    dx = lam[1]-lam[0]
+    a0 = [dx*m0/(np.sqrt(2*np.pi)*sd), m1, sd] #  estimate of 1-gaussian paramters
+
+    #y1g = gaussfit( lam, y, a1g, chisq=chi1g, nterm=3, meas=yerr, est=a0 )
+    a1g,a1cov = curve_fit(single_gauss_func_noder, lam, y, p0=a0)#, bounds=(0, np.inf))
+    y1g = single_gauss_func_noder(lam, *a1g)
+
+    #calculate chi_square
+    #jj = np.where((lam > 1402) & (lam < 1405)) # trim excess zeros in data first
+    jj = np.where(y > 0.001)
+    lam_s = lam[jj]
+    y_s = y[jj]
+    #y_modelone = single_gauss_func_noder(lam_s, *a1g)
+    y_modelone = y1g[jj]
+    yerr_s = yerr[jj]
+    X2one = np.sum(((y_modelone - y_s) / yerr_s)**2)
+
+    #X2_1 = np.sum(((y1g-y)/yerr)**2)
+    chi1g = X2one/(len(y_s)-3) # reduced chi^2
+
+
+
+    # ==== do double-Gaussian fit
+    # estimate parameters
+    a0_2 = est_params([ m0, m1, m2, m3 ], dx=dx)
+    upper_bound = [np.inf,1404,np.inf,np.inf,1404,np.inf]
+    lower_bound = [0,1403,0,0,0,0]
+    a2g, a2cov = curve_fit(double_gauss_func_noder, lam, y, p0=a0_2, sigma = yerr, absolute_sigma = True, maxfev = 110000)#, bounds=(lower_bound, upper_bound)) #,
+
+    # individual gaussians of double fit
+    pars_1 = a2g[0:3]
+    pars_2 = a2g[3:6]
+    y2a = single_gauss_func_noder(lam, *pars_1)
+    y2b = single_gauss_func_noder(lam, *pars_2)
+
+
+    # calculate chi^2
+    #y_modeltwo = double_gauss_func_noder(lam_s, *a2g)
+    y2g = y2a+y2b
+    y_modeltwo = y2g[jj]
+    X2two = np.sum(((y_modeltwo - y_s) / yerr_s)**2)
+    chi2g = X2two/(len(y_modeltwo)-6) # reduced chi^2
+
+    if verbose==True:
+        print('a2g[0] =', a2g[0])
+        print('a1g[0] =', a1g[0])
+        print('chi2g = ', chi2g)
+
+
+
+    # if double fitting WORSE than single gaussian fit, OR OR OR if the amplitude of the second Gaussian is neglible
+    if( chi2g > chi1g ) or (a2g[0] < a1g[0]*0.01):
+        a2g = np.concatenate((a1g, a1g)) #  return copies of single fit params
+        a2g[3] = 0.0 #  but zero amplitude
+        y2a = y1g
+        y2b = 0.0*y1g
+        chi2g = -1.0 #  flag that no fit was attmepted
+
+    if verbose==True:
+        print('a2g = ', a2g)
+        print('chi1g = ',chi1g)
+        print('chi2g = ', chi2g)
+
+
+    d['moms'] = moms
+    d['a1g'] = a1g
+    d['y1g'] = y1g
+    d['a2g'] = a2g
+    d['y2a'] = y2a
+    d['y2b'] = y2b
+    d['chi2g'] = chi2g
+
+    return d
