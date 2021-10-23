b7ece3e42e416b1aee7eb0235fb0f5e63a109276
Will Ashfield <wilashfie@gmail.com>
2021-10-23 14:51:58 -0600

    updated from test_arr

--- ebtel/create_arrays.py
+++ ebtel/create_arrays.py
@@ -7,6 +7,9 @@ from scipy.optimize import curve_fit
 from scipy.signal import savgol_filter
 from matplotlib import gridspec
 
+
+import sys
+sys.path.append("../")
 from fit2gauss import fit2gauss
 
 
@@ -19,13 +22,23 @@ from fit2gauss import fit2gauss
 # log10T/log10G, log of contribution function G and corresponding log of temperature T, imported from dana's email
 
 # set inital values
+line_cgs = 1402.77*1e-8 # cm.
 line = 1402.77
-mass = 28.0*1.66054e-27
-kb = 1.3807e-23
-c = 300
-h = 6.62607e-27
+mass = 28.0*1.66054e-27 # g
+#kb = 1.3807e-23
+kb = 1.3807*1e-16 # cm^2 g /s^2 /K
+#c = 300 # Mm/s
+c = 3.*1e10 # cm /s
+h = 6.62607e-27 # g cm^2 /s
 flux=1.0e3 #? why this val?
-ll = np.arange(line-10,line+10,0.01)
+
+ll = np.arange(line-3,line+3,0.01)
+
+#------------- john
+gamma=5./3.
+k=1.38065e-23
+M=28.09*1.660539040e-27
+c2=299.792
 
 
 class siiv:
@@ -47,19 +60,20 @@ class siiv:
         n_e = tube.tarr.epamu[time]*tube.tarr.rho[time]/1.67e-8 # number density
         b = tube.tarr.b[time]
         dl_e = tube.tarr.dl_e[time]
+        vel = -los_v*1e3
 
         self.t_n = tube.tarr.shape[0] - 1 # len of tube in time (in steps of 0.1s)
 
-        # interpolate our GofT data
-        te = 10**log10T
-        inter = interp1d(te,log10G,kind='cubic', bounds_error=False, fill_value=-10e6) #fill outide vals with large, small number
-        temp = 1e6*tube.tarr.t[time]
-        G = inter(temp)
+        # xlim
+        i_lim = np.where(t>0.04)[0]
+        xl = los_x[i_lim[0]]-0.25
+        xr = xl+1
+        i_min,i_max = i_lim[0]-100,i_lim[0]+100
 
-        # set all log10G with low temp to large, small number (st 10^G~0)
-        t_0 = tube.tarr.t[0]
-        temp_0 = 1e6*np.round(t_0[0],4)
-        G[temp<temp_0] = -10000
+        # scale GofT data
+        te = 10**log10T
+        gT = 10**log10G
+
 
         # nei/eqi arrays at time =time
         f_nei=frac.arrs.f_nei[0]
@@ -69,24 +83,9 @@ class siiv:
 
         temp_fac = f_nei/f_eqi
         np.nan_to_num(temp_fac, copy=False, nan=1); # replace inf values with 1 (due to zeros in eqi)
-        i_half = int(n/2) #[0:i_half] = left half of tube
-        test = f_nei[0:i_half]
-        nei_jj = np.where(test > test[0])
-        nei_jj=nei_jj[0]
 
-        i_half = int(n/2) #[0:i_half] = left half of tube
-        temp_fac = temp_fac[0:i_half]
-        f_jj = np.where(temp_fac > temp_fac[0])
-        f_jj=f_jj[0]
 
-        if (len(f_jj) == 0): f_jj = nei_jj
-
-        # interpolation arrays
-        # define subregion
-        #i_min,i_max = f_jj[0]-30,f_jj[-1]+30 # left half of tube where nei is significant
-        #i_min,i_max = 350,1850 # fixed interval for averaging in time (for n=400)
-        i_min,i_max = 250,i_half-2 # fixed interval for averaging in time (for n=200)
-
+        #sub arrays
         t_s = t[i_min:i_max]
         n_s = len(t_s)
         los_v_s = los_v[i_min:i_max]
@@ -95,9 +94,13 @@ class siiv:
         n_e_s = n_e[i_min:i_max]
         b_s = b[i_min:i_max]
         dl_e_s = dl_e[i_min:i_max]
-        G_s = G[i_min:i_max]
         f_nei_s = f_nei[i_min:i_max]
         f_eqi_s = f_eqi[i_min:i_max]
+
+        #EM
+        A_pixel = 0.029*1e16 # cm^2 -  pixel area: 0.33 x 0.167 arcsec (as seen on sun..)
+        vol_s = dl_e_s*A_pixel*1e8
+        EM_s = vol_s*n_e_s**2
 
         # interpolate
         N=10*n_s
@@ -105,15 +108,15 @@ class siiv:
         ii = np.arange(0,10*(n_s-1))*0.1
         i_length = len(ii)
 
-        int_x = interp1d(i_s,los_x_s,kind=interp)#,fill_value="extrapolate")
-        int_v = interp1d(i_s,sm_v_s,kind=interp)
-        int_t = interp1d(i_s,t_s,kind=interp)
-        int_ne = interp1d(i_s,n_e_s,kind=interp)
-        int_b = interp1d(i_s,b_s,kind=interp)
-        int_dl_e = interp1d(i_s,dl_e_s,kind=interp)
-        int_G = interp1d(i_s,G_s,kind=interp)
-        int_fnei = interp1d(i_s,f_nei_s,kind=interp)
-        int_feqi = interp1d(i_s,f_eqi_s,kind=interp)
+        int_x = interp1d(i_s,los_x_s,kind=interp, bounds_error=False)#,fill_value="extrapolate")
+        int_v = interp1d(i_s,sm_v_s,kind=interp,bounds_error=False)#,fill_value="extrapolate")
+        int_t = interp1d(i_s,t_s,kind=interp,bounds_error=False)
+        int_ne = interp1d(i_s,n_e_s,kind=interp,bounds_error=False)
+        int_b = interp1d(i_s,b_s,kind=interp,bounds_error=False)
+        int_dl_e = interp1d(i_s,dl_e_s,kind=interp,bounds_error=False)
+        int_fnei = interp1d(i_s,f_nei_s,kind=interp,bounds_error=False)
+        int_feqi = interp1d(i_s,f_eqi_s,kind=interp,bounds_error=False)
+        int_em = interp1d(i_s,EM_s,kind=interp,bounds_error=False)
 
         # new, interpolated arrays from tarr/tube
         x = int_x(ii)
@@ -122,29 +125,30 @@ class siiv:
         ne = int_ne(ii)
         B = int_b(ii)
         Dl = int_dl_e(ii)
-        g = int_G(ii)
         nei = int_fnei(ii)
         eqi = int_feqi(ii)
+        em=int_em(ii)
+
+
+        # interpolate G of T data seperatly
+        inter = interp1d(te,gT,kind='linear', bounds_error=False) #fill outide vals with large, small number
+        temp2 = 1e6*T
+        gg = inter(temp2)
+        gg=np.nan_to_num(gg)
+
 
         # factor to multiply GofT by to get actual emission given NEI
         factor = nei/eqi
         np.nan_to_num(factor, copy=False, nan=1); # replace inf values with 1 (due to zeros in eqi)
+        factor[factor>1e10]=1.0 # exclude unnessesarily high values of
 
         # process to create volume given per flux (per Maxwell)
-        rad=np.sqrt(flux/B/np.pi)
-        b_e=0.5*( np.roll(B, -1) + B )
-        A1=rad
-        A2=np.roll(rad,-1)
-        A2[(len(B))-1]=A2[len(B)-2]
-        #volume=(1./3.)*Dl*(A1+A2+np.sqrt(A1*A2))*1e24
-        A = flux/B # replace by area of pixel in Mm (~0.01 Mm^2)
-        A_pixel = 0.029 # Mm^2 -  pixel area: 0.33 x 0.167 arcsec (as seen on sun..)
-        volume = Dl*A_pixel*1e24
+        A_pixel = 0.029*1e16 # cm^2 -  pixel area: 0.33 x 0.167 arcsec (as seen on sun..)
+        volume = Dl*A_pixel*1e8
         EM=volume*ne**2 #calcualte emission measure EM
 
-
         # calculate prefactor to turn intensity into counts
-        photo_erg = h*c*1e6/line*1e10 #erg/photon
+        photo_erg = h*c/line_cgs #erg/photon
         pixel_size = 12.8e-3 # in ˚A [12.8 m˚A (FUV), 25.6 m˚A (NUV)]
         dim = 19e-2
         A_iris = 2.2e-4 # effective area FUV
@@ -155,21 +159,28 @@ class siiv:
 
 
         # determine line broadening given combination of thermal, non-thermal, and instrumental broadenings
-        freq = c/line*1e3
+        freq = c/line_cgs
         v_inst = 3.9 # instrumental broadening (km/s)
-        sig_inst = v_inst/freq
+        sig_inst = v_inst/freq # km
         v_nt = 20.0 # non-thermal broadening (km/s) [De Pontieu et al, 2015]
-        sig_nt = v_nt/freq
-        sig_th = line*np.sqrt(kb*1e6*T/mass)/(c*1e6) # thermal broadening (in ˚A)
-        sig = np.sqrt(sig_th**2+sig_nt**2+sig_inst**2) # total broadening
+        sig_nt = v_nt/freq # km
+        sig_th = line_cgs*np.sqrt(kb*T/mass)/c # thermal broadening (in cm)
+        sig_th = sig_th*1e-5 # km
+        sig = np.sqrt(sig_th**2+sig_nt**2+sig_inst**2) # total broadening km
+        #sig = np.sqrt(sig_th**2+sig_inst**2)
+        sig = sig*1e13 # angstrom
+
+        # quick redefinitions
+        vcm = v*1e8 # vel in cm (to match c in expotential of intensity)
+        crv = np.sqrt(2*np.pi)*sig
 
         # create emission array(s) for each fluid element
-        nn=10*(n_s-1)
+        nn=len(em)
         emissNEI = np.empty([nn,len(ll)])
-        emiss = emissNEI
+        emiss = np.empty([nn,len(ll)])
         for i in range(nn):
-            emissNEI[i,:] = photo_fac*EM[i]*factor[i]*10**g[i]/np.sqrt(2*np.pi)/sig[i]*np.exp(-(ll-line-line*v[i]/c)**2/(2*sig[i]**2))
-            emiss[i,:] = photo_fac*EM[i]*10**g[i]/np.sqrt(2*np.pi)/sig[i]*np.exp(-(ll-line-line*v[i]/c)**2/(2*sig[i]**2))
+            emissNEI[i,:] = photo_fac*em[i]*factor[i]*gg[i]/crv[i]*np.exp(-(ll-line-line*(vcm[i]/c))**2/(2*sig[i]**2))
+            emiss[i,:] = photo_fac*em[i]*gg[i]/crv[i]*np.exp(-(ll-line-line*(vcm[i]/c))**2/(2*sig[i]**2))
 
         # add together all emissions along loop
         tot_emissNEI = np.sum(emissNEI,axis=0)
@@ -195,12 +206,13 @@ class siiv:
             print('area iris = ', A_iris)
             print('atn = ', atn)
             print('photo erg = ', photo_erg)
+            print('testing here!!!!~~~~~')
 
         self.wav = ll
         self.spec = tot_emissNEI
         self.error = meas_error
         self.EM = EM
-        self.g = g
+        self.g = gg
         self.fac = photo_fac*factor
         self.x = x
         self.v = v
@@ -214,15 +226,23 @@ class siiv:
 
     def master(self):
 
-        SPEC = np.zeros((self.t_n,len(self.spec)))
-        ERROR = np.zeros((self.t_n,len(self.error)))
+        i_start = 200
+        i_end = 1000
 
-        for i in range(0,self.t_n):
+        time_len = len(range(i_start,i_end))
+
+
+        SPEC = np.zeros((time_len,len(self.spec)))
+        ERROR = np.zeros((time_len,len(self.error)))
+
+        for i in range(i_start,i_end):
 
             arrs = siiv(TUBE,FRAC,LOG10T,LOG10G,time=i)
 
-            SPEC[i,:] = arrs.spec
-            ERROR[i,:] = arrs.error
+            k = i-i_start
+
+            SPEC[k,:] = arrs.spec
+            ERROR[k,:] = arrs.error
 
         self.total_spec = SPEC
         self.total_error = ERROR
@@ -234,8 +254,8 @@ class siiv:
 
         self.master()
 
-        nu_spec = self.total_spec.reshape(-1,reshape,2000)
-        nu_error = self.total_error.reshape(-1,reshape,2000)
+        nu_spec = self.total_spec.reshape(-1,reshape,600) # length of wavelength array - defined by ll at top.
+        nu_error = self.total_error.reshape(-1,reshape,600)
 
         self.respec = np.mean(nu_spec,axis=1)
         self.reerror = np.mean(nu_error,axis=1)
